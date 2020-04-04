feac36a26b9f3af3f6fc0f831e3b0adfc689dcc8
anaritavalente <59932859+anaritavalente@users.noreply.github.com>
2020-04-04 16:47:17 +0100

    Updated the hour - displaying the input city hour
    Now each time the user gives a city input in the search engine, the webpage displays the hour of that input city, instead of the browser's hour

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-let now = new Date();
+let now = new Date(); 
+// creates a date object with the current date and time 
+//By default JavaScript will use the browser's timezone
 let weekday_number = now.getDay();
 let month_number = now.getMonth();
 let hours = now.getHours();
 let minutes = now.getMinutes();
 
-let week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Monday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "September", "October", "November", "December"];
-let weekday_txt = week[weekday_number-1];
-let month = months[month_number];
+week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+months = ["January", "February", "March", "April", "May", "June", "July", "September", "October", "November", "December"];
 
 if (hours < 10) {
     hours = `0${hours}`;
@@ -15,16 +15,13 @@ if (hours < 10) {
 if (minutes < 10) {
     minutes = `0${minutes}`;
 }
+let hourElement = document.querySelector("#hour");
+hourElement.innerHTML = `${hours}:${minutes}`; 
 
 let dayElement = document.querySelector("#day");
-console.log(dayElement);
-dayElement.innerHTML = `${weekday_txt}, ${now.getDate()} ${month} ${now.getFullYear()}`;
-
-let hourElement = document.querySelector("#hour");
-hourElement.innerHTML = `${hours}:${minutes}`;
+dayElement.innerHTML = `${week[weekday_number-1]}, ${now.getDate()} ${months[month_number]} ${now.getFullYear()}`;
 
 function displayData(response) {
-    console.log(response.data);
     let temperatureElement = document.querySelector("#temp");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -43,7 +40,28 @@ function displayData(response) {
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute('src',`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute('alt',`${response.data.weather[0].description}`);
+   
+    let now = new Date(); 
+    //note that UTC is the reference time zone, from which all time zones are calculated,
+    //and corresponds to the WINTER time in the greenwich meridian
+    let inputcity_timezone_offset = response.data.timezone; //offset in seconds between the input city's timezone and the UTC timezone
+    let hours_of_inputcity = now.setHours(now.getUTCHours() + inputcity_timezone_offset/3600); //returns the miliseconds since January 1, 1970 00:00:00 UTC of the input_city
+    let inputcity_time = new Date(hours_of_inputcity);
+    let hours = inputcity_time.getHours();
+    let minutes = inputcity_time.getMinutes();
+    let dayElement = document.querySelector("#day");
+    dayElement.innerHTML = `${week[inputcity_time.getDay()]}, ${inputcity_time.getDate()} ${months[inputcity_time.getMonth()]} ${inputcity_time.getFullYear()}`;
+    
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let hourElement = document.querySelector("#hour");
+    hourElement.innerHTML = `${hours}:${minutes}`;
 }
+
 function search(city) {
     let apikey= `fab5f60356d4f31a390522bd136e2a65`;
     let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
