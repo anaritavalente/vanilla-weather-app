164848d6c29642b8b286a084429194082c66adc7
anaritavalente <59932859+anaritavalente@users.noreply.github.com>
2020-04-09 15:08:21 +0100

    Added the forecast for the next days
    Also:
    Recovered the "handleSubmit" function
    
    Changed the style of the webpage
    
    Called Search("Lisbon) to display by default the weather of Lisbon, when the user opens the page

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ let months = ["January", "February", "March", "April", "May", "June", "July", "S
 let apiKey = "fab5f60356d4f31a390522bd136e2a65";
 let celsiusTemperature = null;
 
-function formatHours(timestamp) {
-    let now = new Date(timestamp); //By default JavaScript will use the browser's timezone  
+function formatHours(time) {
+    let now = new Date(time); //By default JavaScript will use the browser's timezone  
     let hours = now.getHours();
     let minutes = now.getMinutes();
     if (hours < 10) {
@@ -13,14 +13,12 @@ function formatHours(timestamp) {
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-    let hourElement = document.querySelector("#hour");
-    hourElement.innerHTML = `${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 }
 
-function formatDay(timestamp) {
-    let now = new Date(timestamp);
-    let dayElement = document.querySelector("#day");
-    dayElement.innerHTML = `${week[now.getDay()-1]}, ${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}`;
+function formatDay(time) {
+    let now = new Date(time);
+    return `${week[now.getDay()]}, ${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}`;
 }
 
 function displayData(response) {
@@ -31,6 +29,8 @@ function displayData(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let hourElement = document.querySelector("#hour");
+    let dayElement = document.querySelector("#day");
     
     cityElement.innerHTML = response.data.name;
     temperatureElement.innerHTML = `${celsiusTemperature} ºC`;
@@ -41,31 +41,50 @@ function displayData(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     
     let now = new Date(); 
-    //note that UTC is the reference time zone, from which all time zones are calculated,
-    //and corresponds to the WINTER time in the greenwich meridian
+    //note that UTC is the reference time zone and corresponds to the WINTER time in the greenwich meridian
     let inputcity_timezone_offset = response.data.timezone; //offset in seconds between the input city's timezone and the UTC timezone
     let time_of_inputcity = now.setHours(now.getUTCHours() + inputcity_timezone_offset/3600); //returns the miliseconds since January 1, 1970 00:00:00 UTC of the input_city
-    formatHours(time_of_inputcity);
-    formatDay(time_of_inputcity);
+    hourElement.innerHTML = `${formatHours(time_of_inputcity)}`;
+    dayElement.innerHTML = `${formatDay(time_of_inputcity)}`;
+
     lat = response.data.coord.lat;
     lon = response.data.coord.lon;
-    let forecast_api_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    let forecast_api_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(forecast_api_URL).then(getForecast);
 }
 
-//when user clicks "Search"
-function search() {
-    event.preventDefault();
+function getForecast(response) {
+    for (let i = 1; i < 7; i ++) {
+        forecast = response.data.daily[i];
+        let dayElement = document.querySelector(`#day${i}`);
+        let maxElement = document.querySelector(`#max${i}`);
+        let minElement = document.querySelector(`#min${i}`);
+        let icon = document.querySelector(`#icon${i}`);
+        let next_day = formatDay(forecast.dt*1000);
+        let comma_index = next_day.indexOf(",");
+        
+        dayElement.innerHTML = next_day.slice(0,comma_index);
+        maxElement.innerHTML = `${Math.round(forecast.temp.max)}ºC`;
+        minElement.innerHTML = `${Math.round(forecast.temp.min)}ºC`;
+        icon.setAttribute('src',`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`);
+        icon.setAttribute('alt',`${forecast.weather[0].description}`);
+    }
+}
+
+function search(city) {
     fahrenheitLink.classList.remove("active");
     celsiusLink.classList.add("active");
-    let inputElement = document.querySelector("#city-input");
-    let city = inputElement.value;
     let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiURL).then(displayData);
 }
 
-let formElement = document.querySelector("form");
-formElement.addEventListener("submit", search);
+//when user clicks "Search"
+function handleSubmit(event) {
+    event.preventDefault();
+    let inputElement = document.querySelector("#city-input");
+    let city = inputElement.value;
+    search(city);
+}
 
 //when user clicks "Current location"
 function getMyLocation(position) {
@@ -78,9 +97,6 @@ function getMyLocation(position) {
 function getPosition() {
     navigator.geolocation.getCurrentPosition(getMyLocation);
 }
-
-let currentLocationElement = document.querySelector("#current-position");
-currentLocationElement.addEventListener("click", getPosition);
 
 //when user clicks in the F or ºC links
 function convertToFahrenheit() {
@@ -104,4 +120,10 @@ celsiusLink.addEventListener("click",convertToCelsius);
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click",convertToFahrenheit);
 
-getPosition();
+let currentLocationElement = document.querySelector("#current-position");
+currentLocationElement.addEventListener("click", getPosition);
+
+let formElement = document.querySelector("form");
+formElement.addEventListener("submit", handleSubmit);
+
+search("Lisbon");
